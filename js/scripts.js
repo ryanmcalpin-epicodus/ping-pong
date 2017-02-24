a203c207a4229a808e68cdc0c7b38aadc98c6b53
Ryan McAlpin <rhmcalpin@gmail.com>
2017-02-24 09:32:13 -0800

    replace the number 3 with ping

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 var pingPong = function(numberInput) {
   var numberArray = [];
-  for (var i = 0; i < numberInput; i++) {
+  for (var i = 0; i < numberInput; i++) {   // creates array from 1 to given number
     numberArray.push(i + 1);
   }
-
+  // debugger;
+  numberArray.forEach(function(element, i, array) {
+    if (element === 3) {
+      array.splice(i, 1, "ping");
+    }
+  });
   var result = numberArray;
   return result;
 }
