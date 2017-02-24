4f78f51a4964336e42799c070f3dbe568c9c8c29
Ryan McAlpin <rhmcalpin@gmail.com>
2017-02-24 14:38:25 -0800

    handle empty input field and '0'

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,35 @@
 var pingPong = function(numberInput) {
-  var negative = "";
-  if (numberInput < 0) { //convert negative number input to positive
-    numberInput *= -1;
-    negative = "-"
-  }
-  var numberArray = [];
-  for (var i = 0; i < numberInput; i++) {   // creates array from 1 to given number
-    numberArray.push(i + 1);
-  }
-
-  numberArray.forEach(function(element, i, array) { // replaces multiples of 15, 3, and 5 with "ping-pong", "ping", and "pong", respectively
-    if (element % 15 === 0) {
-      array.splice(i, 1, "ping-pong");
-    } else if (element % 3 === 0) {
-      array.splice(i, 1, "ping");
-    } else if (element % 5 === 0) {
-      array.splice(i, 1, "pong");
-    } else {
-      array.splice(i, 1, negative + element);  //adds "-" to remaining numbers if the input was negative
+  debugger;
+  if (!numberInput) {           //checks for empty input field
+    var result = "Please enter a whole number. You can even try negative numbers!";
+  } else if (numberInput == 0) { //checks for 0
+    var result = "Please enter a whole number other that zero. You can even try negative numbers!";
+  } else {
+    var negative = "";
+    if (numberInput < 0) { //convert negative number input to positive
+      numberInput *= -1;
+      negative = "-"
     }
-  });
 
-  var result = numberArray.join(", ");
+    var numberArray = [];
+    for (var i = 0; i < numberInput; i++) {   // creates array from 1 to given number
+      numberArray.push(i + 1);
+    }
+
+    numberArray.forEach(function(element, i, array) { // replaces multiples of 15, 3, and 5 with "ping-pong", "ping", and "pong", respectively
+      if (element % 15 === 0) {
+        array.splice(i, 1, "ping-pong");
+      } else if (element % 3 === 0) {
+        array.splice(i, 1, "ping");
+      } else if (element % 5 === 0) {
+        array.splice(i, 1, "pong");
+      } else {
+        array.splice(i, 1, negative + element);  //adds "-" to remaining numbers if the input was negative
+      }
+    });
+
+    var result = numberArray.join(", ");
+  }
 
   return result;
 }
@@ -34,6 +42,7 @@ $(function() {
     $("#result").text(result);
     $("#result").slideDown();
   });
+
   $("input").click(function(){
       $("#result").slideUp();
   });
